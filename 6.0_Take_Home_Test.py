d5e0137e816c3449edf831f63009985d14438837
OrigamiElephant <mcculleyezr@urbandaleschools.com>
2019-02-25 12:12:16 -0600

    Finished Test

--- 6.0_Take_Home_Test.py
+++ 6.0_Take_Home_Test.py
@@ -1,22 +1,23 @@
 '''
 HONOR CODE: I solemnly promise that while taking this test I will only use any resources except my brain.
-Signed: ______________________
+Signed: Ezra McCulley
 
 
 Convert Binary 101010 to Hexadecimal.
-
+2A
 Convert Binary 11101001 to Octal.
-
+351
 Convert Hexadecimal FC to Binary.
-
+11111100
 Convert Hexadecimal 1F to Decimal.
-
+11111
 Convert Hexadecimal #FAAFBD to RGB.
-
+R:250 G:175 B:189
 Convert Octal 70 to Binary.
-
+111000
 Convert RGB (32,128,64) to Hexadecimal.
-
+208040
 Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
-
+65,83,67,73,73
+ASCII
 '''
