c1ffe4cde0662c64b8ea9f61bf39630542e9482e
watsoncal <watsoncal@urbandaleschools.com>
2019-11-07 13:52:03 -0600

    Finished Conversions

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name: Cal Watson
 
 
-1.) Convert Binary 101010 to Hexadecimal.
+1.) Convert Binary 101010 to Hexadecimal. 2A
 
-2.) Convert Binary 11101001 to Octal.
+2.) Convert Binary 11101001 to Octal. 351
 
-3.) Convert Hexadecimal FC to Binary.
+3.) Convert Hexadecimal FC to Binary. 11111100
 
-4.) Convert Hexadecimal 1F to Decimal.
+4.) Convert Hexadecimal 1F to Decimal. 31
 
 5.) Convert Hexadecimal #FAAFBD to RGB.
 
-6.) Convert Octal 70 to Binary.
+6.) Convert Octal 70 to Binary. 111000
 
-7.) Convert RGB (32,128,64) to Hexadecimal.
+7.) Convert RGB (32,128,64) to Hexadecimal. #208040
 
-8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
+8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII. ASCII
 
 '''
