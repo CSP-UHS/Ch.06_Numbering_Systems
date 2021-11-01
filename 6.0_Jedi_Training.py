e8c25630094ca77cc2a179ad446ac080baf50fb1
ZTB2021 <blumzac@urbandaleschools.com>
2021-11-01 09:57:14 -0500

    Ch6

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -5,17 +5,17 @@ Sign your name:Zachary Blum
 1.) Convert Binary 101010 to Hexadecimal.
 2 A
 2.) Convert Binary 11101001 to Octal.
-3 6 1
+3 5 1
 3.) Convert Hexadecimal FC to Binary.
 1 1 1 1 1 1 0 0
 4.) Convert Hexadecimal 1F to Decimal.
 31
 5.) Convert Hexadecimal #FAAFBD to RGB.
-
+250,175,190
 6.) Convert Octal 70 to Binary.
-111000
+1 1 1 0 0 0
 7.) Convert RGB (32,128,64) to Hexadecimal.
-
+20 80 40
 8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
-            A           S           C       I           I
+            A         S         C         I         I
 '''
