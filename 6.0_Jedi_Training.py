12c5ecdf24c74cda24206eca2f0c9388643ec992
fletchermat <fletchermat@urbandaleschools.com>
2022-11-04 08:57:31 -0500

    Conversions

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -1,21 +1,22 @@
 '''
-Sign your name:________________
+Sign your name:Matthew Fletcher
 
-
-1.) Convert Binary 101010 to Hexadecimal.
-
-2.) Convert Binary 11101001 to Octal.
-
+8421
+1.) Convert Binary 101010 to Hexadecimal.   0010 1010
+2A
+2.) Convert Binary 11101001 to Octal.     011 101 001
+351
 3.) Convert Hexadecimal FC to Binary.
-
+11111100
 4.) Convert Hexadecimal 1F to Decimal.
-
+31
 5.) Convert Hexadecimal #FAAFBD to RGB.
-
+RGB(250,175,189)
 6.) Convert Octal 70 to Binary.
-
+111000
 7.) Convert RGB (32,128,64) to Hexadecimal.
-
-8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
-
+#208040
+8.) Convert 0100 0001, 0101 0011, 0100 0011, 0100 1001, 0100 1001 to ASCII.
+ hex          41          53          43         49         49
+       ASCII
 '''
